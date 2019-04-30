e936d38d0fc4c79275e423ba76df75db109e5f76
Dell <bparekh@cpp.edu>
2019-04-30 16:49:05 -0700

    added 2018's question code

--- src/main/java/cpp/hackerrank/controller/HackerrankDataController.java
+++ src/main/java/cpp/hackerrank/controller/HackerrankDataController.java
@@ -102,7 +102,11 @@ public class HackerrankDataController {
 			}
 
 			response.put("success", true);
-			response.put("message", allCandidateList);
+			if(allCandidateList.size() > 30) {
+				response.put("message", allCandidateList.subList(0, 30));
+			} else {
+				response.put("message", allCandidateList);
+			}
 		}catch(Exception e) {
 			e.printStackTrace();
 			response.put("success", false);
--- src/main/java/cpp/hackerrank/util/QuestionPointMap.java
+++ src/main/java/cpp/hackerrank/util/QuestionPointMap.java
@@ -8,7 +8,7 @@ public class QuestionPointMap {
 	private static Map<String, Double> mapper = new HashMap<String, Double>();
 
 	static {
-		//TestID: 142875
+		//TestID: 142875 (HSPC 2019 Sample Contest)
 		mapper.put("133529", 50.0);
 		mapper.put("130309", 50.0);
 		mapper.put("130302", 50.0);
@@ -17,6 +17,26 @@ public class QuestionPointMap {
 		mapper.put("157956", 75.0);
 		mapper.put("111168", 75.0);
 		mapper.put("222917", 75.0);
+
+		//TestID: 231089 (California HSPC 2018 (HS I))
+		mapper.put("463742", 50.0);
+		mapper.put("467189", 50.0);
+		mapper.put("467186", 50.0);
+		mapper.put("463741", 50.0);
+		mapper.put("463766", 50.0);
+		mapper.put("463977", 50.0);
+		mapper.put("463770", 50.0);
+		mapper.put("463966", 50.0);
+
+		//TestID: 235052 (California HSPC 2018 (HS Team))
+		mapper.put("463742", 50.0);
+		mapper.put("467189", 50.0);
+		mapper.put("467186", 50.0);
+		mapper.put("463741", 50.0);
+		mapper.put("463766", 50.0);
+		mapper.put("463977", 50.0);
+		mapper.put("463770", 50.0);
+		mapper.put("463966", 50.0);
 	}
 
 	public static Map<String, Double> getMapper() {
