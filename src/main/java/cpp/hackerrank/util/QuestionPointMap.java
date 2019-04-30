c0d3e75b614712af353e430a1fa4399813804612
Dell <bparekh@cpp.edu>
2019-04-29 20:41:24 -0700

    removed unused part

--- src/main/java/cpp/hackerrank/util/QuestionPointMap.java
+++ src/main/java/cpp/hackerrank/util/QuestionPointMap.java
@@ -17,16 +17,6 @@ public class QuestionPointMap {
 		mapper.put("157956", 75.0);
 		mapper.put("111168", 75.0);
 		mapper.put("222917", 75.0);
-
-		//TestID: 231089
-		mapper.put("133529", 50.0);
-		mapper.put("130309", 50.0);
-		mapper.put("130302", 50.0);
-		mapper.put("130306", 50.0);
-		mapper.put("232646", 50.0);
-		mapper.put("157956", 75.0);
-		mapper.put("111168", 75.0);
-		mapper.put("222917", 75.0);
 	}
 
 	public static Map<String, Double> getMapper() {
