47d638e28fd916f9bbf0c4acba3859db485cdf31
Dell <bparekh@cpp.edu>
2019-05-01 12:41:09 -0700

    change comparing order

--- src/main/java/cpp/hackerrank/util/CandidateComparator.java
+++ src/main/java/cpp/hackerrank/util/CandidateComparator.java
@@ -8,26 +8,27 @@ public class CandidateComparator implements Comparator<Candidate>{
 
 	@Override
 	public int compare(Candidate o1, Candidate o2) {
-		
+
 		//Compare final score first
-        if (o1.getFinalScore() != o2.getFinalScore())
-        {
-            return o1.getFinalScore() < o2.getFinalScore() ? 1 : -1;
-        }
+		if (o1.getFinalScore() != o2.getFinalScore())
+		{
+			return o1.getFinalScore() < o2.getFinalScore() ? 1 : -1;
+		}
 
-        //If final scores are same then compare time takem
-        if (o1.getTimeTaken() != o2.getTimeTaken())
-        {
-        	return o1.getTimeTaken() > o2.getTimeTaken() ? 1 : -1;
-        }
+		//If time taken is also same then compare score
+		if (o1.getScore() != o2.getScore())
+		{
+			return o1.getScore() < o2.getScore() ? 1 : -1;
+		}
 
-        //If time taken is also same then compare score
-        if (o1.getScore() != o2.getScore())
-        {
-        	return o1.getScore() < o2.getScore() ? 1 : -1;
-        }
-        // Finally by email id
-        return o1.getEmail().compareTo(o2.getEmail());
+		//If final scores are same then compare time takem
+		if (o1.getTimeTaken() != o2.getTimeTaken())
+		{
+			return o1.getTimeTaken() > o2.getTimeTaken() ? 1 : -1;
+		}
+
+		// Finally by email id
+		return o1.getEmail().compareTo(o2.getEmail());
 	}
 
 }
